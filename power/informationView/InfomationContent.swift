efdd21fde56097d54099e2542cebe0fdbecb1bb5
IJayHuI <2949763264@qq.com>
2025-06-27 22:27:27 +0800

    update: 在设备详情视图中添加“详细信息”按钮，优化信息展示逻辑

--- power/informationView/InfomationContent.swift
+++ power/informationView/InfomationContent.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct InfomationContent: View {
     let inputDevice: InformationViewDevice?
+    @State private var showModal = false
     var body: some View {
         VStack(spacing: 15){
             VStack{
@@ -40,36 +41,6 @@ struct InfomationContent: View {
                 }
             }
             VStack{
-                if let param = inputDevice?.size,!param.isEmpty {
-                    Text("尺寸")
-                        .font(.title2)
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("\(param)")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-            }
-            VStack{
-                if let param = inputDevice?.weight,!param.isNaN {
-                    Text("重量")
-                        .font(.title2)
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("\(param)")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-            }
-            VStack{
-                if let param = inputDevice?.capacity,!param.isEmpty {
-                    Text("容量")
-                        .font(.title2)
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("\(param)")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                }
-            }
-            VStack{
                 if let param = inputDevice?.type,!param.isEmpty {
                     Text("类型")
                         .font(.title2)
@@ -92,59 +63,107 @@ struct InfomationContent: View {
                         }
                         .frame(maxWidth: .infinity,alignment: .leading)
                     }
-
+                    
                 }
             }
-            VStack{
-                if let param = inputDevice?.input,!param.isEmpty {
-                    Text("输入功率")
-                        .font(.title2)
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    ScrollView(.horizontal){
-                        HStack() {
-                            ForEach(param, id: \.self) { item in
-                                Button(
-                                    action: {
-                                        
-                                    }) {
-                                        Text(item)
-                                            .padding(10)
-                                    }
-                                    .background(.gray.opacity(0.2))
-                                    .cornerRadius(30)
-                                    .foregroundStyle(.primary)
-                            }
-                        }
-                        .frame(maxWidth: .infinity,alignment: .leading)
+            VStack {
+                Button(
+                    action: {
+                        showModal = true
+                    }) {
+                        Text("详细信息")
+                            .padding(10)
+                    }
+            }
+        }
+        .sheet(isPresented: $showModal) {
+            VStack(spacing: 15){
+                VStack{
+                    if let param = inputDevice?.size,!param.isEmpty {
+                        Text("尺寸")
+                            .font(.title2)
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("\(param)")
+                            .frame(maxWidth: .infinity, alignment: .leading)
                     }
                 }
-            }
-            VStack{
-                if let param = inputDevice?.output,!param.isEmpty {
-                    Text("输出功率")
-                        .font(.title2)
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    ScrollView(.horizontal){
-                        HStack() {
-                            ForEach(param, id: \.self) { item in
-                                Button(
-                                    action: {
-                                        
-                                    }) {
-                                        Text(item)
-                                            .padding(10)
-                                    }
-                                    .background(.gray.opacity(0.2))
-                                    .cornerRadius(30)
-                                    .foregroundStyle(.primary)
-                            }
-                        }
-                        .frame(maxWidth: .infinity,alignment: .leading)
+                VStack{
+                    if let param = inputDevice?.weight,!param.isNaN {
+                        Text("重量")
+                            .font(.title2)
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("\(param)")
+                            .frame(maxWidth: .infinity, alignment: .leading)
                     }
                 }
+                VStack{
+                    if let param = inputDevice?.capacity,!param.isEmpty {
+                        Text("容量")
+                            .font(.title2)
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("\(param)")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                VStack{
+                    if let param = inputDevice?.input,!param.isEmpty {
+                        Text("输入功率")
+                            .font(.title2)
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        ScrollView(.horizontal){
+                            HStack() {
+                                ForEach(param, id: \.self) { item in
+                                    Button(
+                                        action: {
+                                            
+                                        }) {
+                                            Text(item)
+                                                .padding(10)
+                                        }
+                                        .background(.gray.opacity(0.2))
+                                        .cornerRadius(30)
+                                        .foregroundStyle(.primary)
+                                }
+                            }
+                            .frame(maxWidth: .infinity,alignment: .leading)
+                        }
+                    }
+                }
+                
+                VStack{
+                    if let param = inputDevice?.output,!param.isEmpty {
+                        Text("输出功率")
+                            .font(.title2)
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        ScrollView(.horizontal){
+                            HStack() {
+                                ForEach(param, id: \.self) { item in
+                                    Button(
+                                        action: {
+                                            
+                                        }) {
+                                            Text(item)
+                                                .padding(10)
+                                        }
+                                        .background(.gray.opacity(0.2))
+                                        .cornerRadius(30)
+                                        .foregroundStyle(.primary)
+                                }
+                                .frame(maxWidth: .infinity,alignment: .leading)
+                            }
+                        }
+                    }
+                }
+                
             }
+            .padding()
+            .frame(maxHeight: .infinity, alignment: .top)
+            .presentationDetents([.medium,.large])
         }
         .padding()
     }
