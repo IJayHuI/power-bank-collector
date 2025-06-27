83ebaa2125547a9f95ea30f79d45ed71ce398ec3
WeiAnUppp <weianuen@icloud.com>
2025-06-27 19:29:06 +0800

    update 更新设置页

--- power/tabView/SettingView.swift
+++ power/tabView/SettingView.swift
@@ -7,18 +7,56 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct SettingView: View {
     var body: some View {
         NavigationStack {
-            VStack {
-                Image(systemName: "gear")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("设置页内容")
+            VStack(spacing: 20) {
+                HStack {
+                    Text("设置")
+                        .font(.largeTitle)
+                        .bold()
+                    Spacer()
+                }
+                .padding(.horizontal)
+
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.gray.opacity(0.3))
+                    .frame(height: 150)
+                    .padding(.horizontal)
+
+                // 按钮列表
+                VStack(spacing: 12) {
+                    ForEach(["维护", "拥有", "收藏", "愿望"], id: \.self) { title in
+                        Button(action: {
+                            // 点击事件
+                        }) {
+                            HStack {
+                                    Text(title)
+                                        .font(.headline)
+                                            Spacer()
+                                    }
+                                    .padding()
+                                    .background(Color.gray.opacity(0.3))
+                                    .cornerRadius(20)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+                .padding(.horizontal)
+
+                Spacer()
+
             }
-            .navigationTitle("设置")
+            .navigationTitle("")
+            .navigationBarHidden(true)
         }
     }
+}
+
+#Preview {
+    SettingView()
 }
 
 #Preview {
